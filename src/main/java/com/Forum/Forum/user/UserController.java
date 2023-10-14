778392d0bca7cafa21efe908dc1682e3614d3ce1
Elliott Larsen <elliottlrsn@gmail.com>
2023-10-13 21:44:59 -0600

    Registration added.

--- src/main/java/com/Forum/Forum/user/UserController.java
+++ src/main/java/com/Forum/Forum/user/UserController.java
@@ -4,29 +4,39 @@ import jakarta.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/user")
 public class UserController {
+
     private final UserService userService;
 
-    // TODO: Return the user object.
-    @PostMapping(value = "/user/signup/")
-    @ResponseBody
+    @GetMapping("/signup")
+    public String signup(UserCreateForm userCreateForm) {
+        return "signup_form";
+    }
+
+    @PostMapping("/signup")
     public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "ERROR";
+            return "signup_form";
         }
 
         if (!userCreateForm.getPassword().equals(userCreateForm.getPasswordConfirm())) {
-            bindingResult.rejectValue("password", "passwordConfirm", "Passwords do not match.");
-            return "Error";
+            bindingResult.rejectValue("passwordConfirm", "passwordInCorrect",
+                    "Passwords do not match.");
+            return "signup_form";
         }
-        userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword());
-        return "Success";
+
+        userService.create(userCreateForm.getUsername(),
+                userCreateForm.getEmail(), userCreateForm.getPassword());
+
+        return "redirect:/";
     }
 }
