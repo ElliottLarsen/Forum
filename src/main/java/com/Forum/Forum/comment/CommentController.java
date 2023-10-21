c604c73b6f5b491ce043dd32a88e3ff81a911581
Elliott Larsen <elliottlrsn@gmail.com>
2023-10-21 14:01:45 -0600

    Edit and delete for comments.

--- src/main/java/com/Forum/Forum/comment/CommentController.java
+++ src/main/java/com/Forum/Forum/comment/CommentController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.server.ResponseStatusException;
 import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
 import org.springframework.ui.Model;
@@ -34,5 +37,41 @@ public class CommentController {
         }
         this.commentService.create(post, commentCreateForm.getContent(), siteUser);
         return String.format("redirect:/post/detail/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/comment/modify/{id}")
+    public String commentModify(CommentCreateForm commentForm, @PathVariable("id") Integer id, Principal principal) {
+        Comment comment = this.commentService.getComment(id);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You are not authorized to edit this.");
+        }
+        commentForm.setContent(comment.getContent());
+        return "comment_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/comment/modify/{id}")
+    public String commentModify(@Valid CommentCreateForm commentForm, BindingResult bindingResult, @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "comment_form";
+        }
+        Comment comment = this.commentService.getComment(id);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You are not authorized to edit this.");
+        }
+        this.commentService.modify(comment, commentForm.getContent());
+        return String.format("redirect:/post/detail/%s", comment.getPost().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/comment/delete/{id}")
+    public String commentDelete(Principal principal, @PathVariable("id") Integer id) {
+        Comment comment = this.commentService.getComment(id);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "You are not authorized to delete this.");
+        }
+        this.commentService.delete(comment);
+        return String.format("redirect:/post/detail/%s", comment.getPost().getId());
     }
 }
--- src/main/java/com/Forum/Forum/comment/CommentService.java
+++ src/main/java/com/Forum/Forum/comment/CommentService.java
@@ -4,7 +4,8 @@ import com.Forum.Forum.post.Post;
 import com.Forum.Forum.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import java.util.Optional;
+import com.Forum.Forum.DataNotFoundException;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
@@ -20,5 +21,24 @@ public class CommentService {
         comment.setAuthor(author);
         this.commentRepository.save(comment);
         return comment;
+    }
+
+    public Comment getComment(Integer id) {
+        Optional<Comment> comment = this.commentRepository.findById(id);
+        if (comment.isPresent()) {
+            return comment.get();
+        } else {
+            throw new DataNotFoundException("Comment not found.");
+        }
+    }
+
+    public void modify(Comment comment, String content) {
+        comment.setContent(content);
+        comment.setModifyDate(LocalDateTime.now());
+        this.commentRepository.save(comment);
+    }
+
+    public void delete(Comment comment) {
+        this.commentRepository.delete(comment);
     }
 }
